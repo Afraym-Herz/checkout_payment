f121f1be745eff0b18139b3eb9b0166c9606b8d2
Afraym-Herz <afraym100herz@gmail.com>
2025-07-03 00:58:14 +0300

    create payment gateway bottom sheet and ignore our credit card

--- lib/features/presentation/views/widgets/my_cart_view_body.dart
+++ lib/features/presentation/views/widgets/my_cart_view_body.dart
@@ -2,6 +2,8 @@ import 'package:checkout_payment/core/utils/styles.dart';
 import 'package:checkout_payment/features/presentation/views/payment_view.dart';
 import 'package:checkout_payment/features/presentation/views/widgets/custom_button.dart';
 import 'package:checkout_payment/features/presentation/views/widgets/order_info_item.dart';
+import 'package:checkout_payment/features/presentation/views/widgets/payment_gateway_bottom_sheet.dart';
+import 'package:checkout_payment/features/presentation/views/widgets/payment_gateway_list.dart';
 import 'package:flutter/material.dart';
 
 class MyCartViewBody extends StatelessWidget {
@@ -17,43 +19,51 @@ class MyCartViewBody extends StatelessWidget {
           const Column(
             children: [
               OrderInfoItem(
-            productName: "Order Subtotal",
-            titleStyle: Styles.style18,
-            subTitle: r'$42.97',
-            subTitleStyle: Styles.style18,
-          ),
-          SizedBox(height: 3),
-          OrderInfoItem(
-            productName: "Discount",
-            titleStyle: Styles.style18,
-            subTitle: r'$0',
-            subTitleStyle: Styles.style18,
-          ),
-          SizedBox(height: 3),
-          OrderInfoItem(
-            productName: 'Shipping',
-            titleStyle: Styles.style18,
-            subTitle: r'$8',
-            subTitleStyle: Styles.style18,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Divider(color: Color(0xffC7C7C7), thickness: 2, height: 34),
-          ),
-          OrderInfoItem(
-            productName: "Total",
-            titleStyle: Styles.style24,
-            subTitle: r"$50.97",
-            subTitleStyle: Styles.style24,
-          ),
+                productName: "Order Subtotal",
+                titleStyle: Styles.style18,
+                subTitle: r'$42.97',
+                subTitleStyle: Styles.style18,
+              ),
+              SizedBox(height: 3),
+              OrderInfoItem(
+                productName: "Discount",
+                titleStyle: Styles.style18,
+                subTitle: r'$0',
+                subTitleStyle: Styles.style18,
+              ),
+              SizedBox(height: 3),
+              OrderInfoItem(
+                productName: 'Shipping',
+                titleStyle: Styles.style18,
+                subTitle: r'$8',
+                subTitleStyle: Styles.style18,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Divider(
+                  color: Color(0xffC7C7C7),
+                  thickness: 2,
+                  height: 34,
+                ),
+              ),
+              OrderInfoItem(
+                productName: "Total",
+                titleStyle: Styles.style24,
+                subTitle: r"$50.97",
+                subTitleStyle: Styles.style24,
+              ),
             ],
           ),
           const SizedBox(height: 16),
           CustomButton(
             title: 'Complete Payment',
-            onTap: () {              
-              Navigator.of(context).push( MaterialPageRoute(builder: (context) {return const PaymentView(); }
-              ),
+            onTap: () {
+              //  Navigator.of(context).push( MaterialPageRoute(builder: (context) {return const PaymentView();}),);
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return const PaymentGatewayMethodsBottomSheet();
+                },
               );
             },
           ),
