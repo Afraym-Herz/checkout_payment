cd45722af744c8c1228cfa2aa6d2f0494c7ab9a4
Afraym-Herz <afraym100herz@gmail.com>
2025-07-05 23:09:02 +0300

    create api sevices file and stripe services

--- lib/core/utils/stripe_services.dart
+++ lib/core/utils/stripe_services.dart
@@ -1,23 +1,41 @@
+import 'package:checkout_payment/core/utils/api_keys.dart';
+import 'package:checkout_payment/core/utils/api_services.dart';
+import 'package:checkout_payment/features/checkout_feature/data/models/payment_intent_model/payment_intent_model.dart';
 import 'package:checkout_payment/features/checkout_feature/data/models/payment_intent_model_input.dart';
-import 'package:dio/dio.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 class StripeServices {
-  final Dio dio = Dio();
+  ApiServices apiServices = ApiServices();
 
-  Future<Response> post ({
-    required String url,
-    required body,
-    required String token,
-    String? contentType,
-  }) async {
-    var response = await dio.post(
-      url,
-      data: body,
-      options: Options(
-        contentType: contentType,
-        headers: {"Authorization": "Bearer $token"},
-      ),
+  Future<PaymentIntentModel> createPaymentIntent(
+    PaymentIntentModelInput paymentIntentModelInput,
+  ) async {
+    var response = await apiServices.post(
+      url: "https://api.stripe.com/v1/payment_intents",
+      body: paymentIntentModelInput.toJson() ,
+      token: ApiKeys.secretKey,
     );
-    return response ;
+
+    return PaymentIntentModel.fromJson(response.data);
   }
+
+  Future initPaymentSheet ({required String paymentIntentClientSecret}) async {
+    Stripe.instance.initPaymentSheet(paymentSheetParameters: SetupPaymentSheetParameters(
+      paymentIntentClientSecret: paymentIntentClientSecret ,
+      merchantDisplayName: "Afraym Herz" ,
+    ));
+  }
+
+  Future displayPaymentSheet () async {
+    Stripe.instance.presentPaymentSheet() ;
+  }
+
+  Future makePayment ({required PaymentIntentModelInput paymentIntentModelInput}) async {
+    
+    var paymentIntentModel = await createPaymentIntent(paymentIntentModelInput);
+    await initPaymentSheet(paymentIntentClientSecret: paymentIntentModel.clientSecret! );
+    await displayPaymentSheet() ;
+    
+  }
+
 }
