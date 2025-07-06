2abb8773c654143111180bee57088ba723b99b89
Afraym-Herz <afraym100herz@gmail.com>
2025-07-06 11:43:11 +0300

    create stripe payment cubit

---
+++ lib/features/checkout_feature/presentation/manager/cubit/stripe_payment_cubit.dart
@@ -0,0 +1,22 @@
+
+import 'package:checkout_payment/features/checkout_feature/data/models/payment_intent_model_input.dart';
+import 'package:checkout_payment/features/checkout_feature/data/repos/checkout_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'stripe_payment_state.dart';
+
+class StripePaymentCubit extends Cubit<StripePaymentState> {
+  StripePaymentCubit(this.checkoutRepo) : super(StripePaymentInitial());
+
+  final CheckoutRepo checkoutRepo ;
+
+  Future makePayment ({required PaymentIntentModelInput paymentIntentModelInput }) async {
+
+    var data = await checkoutRepo.makePayment(paymentIntentModelInput: paymentIntentModelInput);
+
+     data.fold(
+      (l) => emit(StripePaymentFailure(errMessage: l.errMessage)), 
+      (r) => emit(StripePaymentSuccess()));
+  }
+}
